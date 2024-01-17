b1e216327d1af5905c0638551adb24b043ad79ba
khalilweeb <khalilazaez@gmail.com>
2024-01-17 23:08:26 +0100

    adding folders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import Canva from "./components/Canva";
+import PieChart from "./components/Pie";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+       <Canva/>
+       <hr></hr>
+       <PieChart></PieChart>
+      
     </div>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,11 @@
+ const  data = [
+    {month : 1 , gained: 1000},
+    {month : 2 , gained: 300},
+    {month : 3 , gained: 600},
+    {month : 4 , gained: 1000},
+    {month : 5 , gained: 1200},
+    {month : 6 , gained: 800},
+];
+
+
+export default data;
